0079e8e8e467959ac958b20c8cf29bcade699c6d
Olaf <olaf.chuszno@gmail.com>
2024-09-08 16:56:53 +0200

    Disable scrolling when  menu is open

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,9 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DropdownMenu from '../DropdownMenu/DropdownMenu.tsx';
 import './Menu.scss';
  
 const Menu: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+
+      document.body.classList.add('no-scroll')
+    } else {
+      document.body.classList.remove('no-scroll')
+    }
+
+    return () => {
+      document.body.classList.remove('no-scroll')
+    }
+  }, [isMenuOpen])
 
   return (<div>
     <button className='menu-button' onClick={() => {setIsMenuOpen((current) => !current)}}>
