4669bc7c348bac081e5b19337ce6e38512e18d4b
Olaf <olaf.chuszno@gmail.com>
2024-11-27 17:00:04 +0100

    Implement styled components in Menu

---
+++ src/components/Menu/Menu.parts.tsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+const developmentUrlHeading = 'http://localhost:3000/smart-it';
+
+export const MenuButton = styled.button<{ isProduction: boolean }>`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  height: 40px;
+  width: 40px;
+  background-image: ${({ isProduction }) =>
+    isProduction
+      ? `url('/icons/burger-menu.svg')`
+      : `url(${developmentUrlHeading}/icons/burger-menu.svg)`};
+  transition: 0.4s all ease;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+
+  &:hover {
+    background-image: ${({ isProduction }) =>
+      isProduction
+        ? `url('/icons/burger-menu-hover.svg')`
+        : `url(${developmentUrlHeading}/icons/burger-menu-hover.svg)`};
+    transition: 0.4s all ease;
+  }
+
+  &:focus {
+    transform: translateX(5px);
+    transition: 0.4s all ease;
+  }
+`;
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,33 +1,48 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import DropdownMenu from '../DropdownMenu/DropdownMenu.tsx';
+import { ProductionEnvironmentContext } from '../EnvironmentProvider.tsx';
+import * as P from './Menu.parts.tsx';
 import './Menu.scss';
- 
+
 const Menu: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const isProduction = useContext(ProductionEnvironmentContext);
 
   useEffect(() => {
     if (isMenuOpen) {
       window.scrollTo({ top: 0, behavior: 'smooth' });
 
-      document.body.classList.add('menu-open')
-      document.querySelectorAll('.filter').forEach(el => el.classList.add('visually-hidden'));
+      document.body.classList.add('menu-open');
+      document
+        .querySelectorAll('.filter')
+        .forEach((el) => el.classList.add('visually-hidden'));
     } else {
-      document.body.classList.remove('menu-open')
-      document.querySelectorAll('.filter').forEach(el => el.classList.remove('visually-hidden'));
+      document.body.classList.remove('menu-open');
+      document
+        .querySelectorAll('.filter')
+        .forEach((el) => el.classList.remove('visually-hidden'));
     }
 
     return () => {
-      document.body.classList.remove('menu-open')
-      document.querySelectorAll('.filter').forEach(el => el.classList.remove('visually-hidden'));
-    }
-  }, [isMenuOpen])
+      document.body.classList.remove('menu-open');
+      document
+        .querySelectorAll('.filter')
+        .forEach((el) => el.classList.remove('visually-hidden'));
+    };
+  }, [isMenuOpen]);
 
-  return (<div className='menu-container'>
-    <button className='menu-button' onClick={() => {setIsMenuOpen((current) => !current)}}>
-    </button>
+  return (
+    <div>
+      <P.MenuButton
+        onClick={() => {
+          setIsMenuOpen((current) => !current);
+        }}
+        isProduction={isProduction}
+      />
 
-    {isMenuOpen && <DropdownMenu setIsMenuOpen={setIsMenuOpen} />}
-  </div>);
-}
- 
+      {isMenuOpen && <DropdownMenu setIsMenuOpen={setIsMenuOpen} />}
+    </div>
+  );
+};
+
 export default Menu;
