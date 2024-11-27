857e2520128dfaffe9d35a3bcbe7ab8ea5b66a4c
Olaf <olaf.chuszno@gmail.com>
2024-11-27 17:02:19 +0100

    Extract header links to a constant

--- src/components/NavLinks/NavLinks.tsx
+++ src/components/NavLinks/NavLinks.tsx
@@ -1,42 +1,12 @@
 import React from 'react';
 import * as P from './NavLinks.parts.tsx';
 import './NavLinks.scss';
+import headerLinks from '../../constants/HeaderLinks.ts';
 
 export const NavLinks: React.FC = () => {
-  const links = [
-    {
-      href: 'https://www.smart-it.com/pl/industries/',
-      text: 'Usługi',
-    },
-    {
-      href: 'https://www.smart-it.com/pl/industries/',
-      text: 'Produkty',
-    },
-    {
-      href: 'https://www.smart-it.com/pl/industries/',
-      text: 'Klienci',
-    },
-    {
-      href: 'https://www.smart-it.com/pl/industries/',
-      text: 'Firma',
-    },
-    {
-      href: 'https://www.smart-it.com/pl/industries/',
-      text: 'Skontaktuj się z nami',
-    },
-    {
-      href: 'https://www.smart-it.com/pl/industries/',
-      text: 'Oferty pracy',
-    },
-    {
-      href: 'https://www.smart-it.com/pl/industries/',
-      text: 'Staż',
-    },
-  ];
-
   return (
     <>
-      {links.map((link) => (
+      {headerLinks.map((link) => (
         <P.NavLink key={link.text} href={link.href}>{link.text}</P.NavLink>
       ))}
     </>
---
+++ src/constants/HeaderLinks.ts
@@ -0,0 +1,32 @@
+const headerLinks = [
+  {
+    href: 'https://www.smart-it.com/pl/industries/',
+    text: 'Usługi',
+  },
+  {
+    href: 'https://www.smart-it.com/pl/industries/',
+    text: 'Produkty',
+  },
+  {
+    href: 'https://www.smart-it.com/pl/industries/',
+    text: 'Klienci',
+  },
+  {
+    href: 'https://www.smart-it.com/pl/industries/',
+    text: 'Firma',
+  },
+  {
+    href: 'https://www.smart-it.com/pl/industries/',
+    text: 'Skontaktuj się z nami',
+  },
+  {
+    href: 'https://www.smart-it.com/pl/industries/',
+    text: 'Oferty pracy',
+  },
+  {
+    href: 'https://www.smart-it.com/pl/industries/',
+    text: 'Staż',
+  },
+];
+
+export default headerLinks;
