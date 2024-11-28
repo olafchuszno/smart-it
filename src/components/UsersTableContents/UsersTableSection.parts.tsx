c481b05098eb752bca58c49b02536704f16ad43e
Olaf <olaf.chuszno@gmail.com>
2024-11-28 16:24:36 +0100

    Extract UsersTableSection component

---
+++ src/components/UsersTableContents/UsersTableSection.parts.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const SpinnerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const NoUsersResultMessage = styled.p`
+  padding: 8px 16px;
+  background-color: #86bf2b;
+  color: white;
+  font-weight: 600;
+  border-radius: 10px;
+`;
---
+++ src/components/UsersTableContents/UsersTableSection.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { LoadingSpinner } from '../LoadingSpinner.jsx';
+import { UsersTableUI } from '../UsersTableUI/UsersTableUI.tsx';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../app/store.ts';
+import { UsersStatus } from '../../features/users.ts';
+import * as P from './UsersTableSection.parts.tsx';
+
+const UsersTableContents = () => {
+  const { status } = useSelector((state: RootState) => state.users);
+
+  const { sortedUsers: users } = useSelector((state: RootState) => state.users);
+
+  const isFetchingUsers = status === UsersStatus.Fetching;
+  const didUsersFetchingFailed = status === UsersStatus.Error;
+  const hasAnyUsers = !!users.length;
+
+  return (
+    <>
+      {didUsersFetchingFailed && <p>Could not fetch the users</p>}
+
+      {isFetchingUsers && (
+        <P.SpinnerContainer>
+          <LoadingSpinner />
+        </P.SpinnerContainer>
+      )}
+
+      {!isFetchingUsers &&
+        (hasAnyUsers ? (
+          <UsersTableUI users={users} />
+        ) : (
+          <P.NoUsersResultMessage>
+            Nie znaleziono użytkownika.
+          </P.NoUsersResultMessage>
+        ))}
+    </>
+  );
+};
+
+export default UsersTableContents;
