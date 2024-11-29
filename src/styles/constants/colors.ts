b07a1c742a0fab13a6df8ed0be1a4140948656f9
Olaf <olaf.chuszno@gmail.com>
2024-11-29 18:39:31 +0100

    Add basic colors configuration to constants

--- src/styles/constants/colors.ts
+++ src/styles/constants/colors.ts
@@ -1,5 +1,10 @@
 export const colors = {
-  white: '#fff',
+  white: {
+    main: '#fff',
+  },
+  black: {
+    main: '#000',
+  },
   green: {
     main: 'rgb(134, 191, 43)',
     lighter: '#86bf2b26'
