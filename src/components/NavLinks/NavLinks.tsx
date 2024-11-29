bd0792e9f0b487769f1c523101d1080283b20a97
Olaf <olaf.chuszno@gmail.com>
2024-11-29 12:41:28 +0100

    Update header links for language and auth implementation

--- src/components/NavLinks/NavLinks.tsx
+++ src/components/NavLinks/NavLinks.tsx
@@ -1,16 +1,43 @@
 import React from 'react';
+import LanguageMenu from '../LanguageMenu/LanguageMenu.tsx';
+import styled from 'styled-components';
+import headerLinks from '../../constants/HeaderLinks.ts';
+import { useTranslation } from 'react-i18next';
 import * as P from './NavLinks.parts.tsx';
 import './NavLinks.scss';
-import headerLinks from '../../constants/HeaderLinks.ts';
-import LanguageMenu from '../LanguageMenu/LanguageMenu.tsx';
+
+const SessionButtons = () => {
+  const { t } = useTranslation();
+
+  // TODO add Sign out &/ register
+
+  return <span>
+    <button onClick={() => {}}>{t('headerLinks.logIn')}</button>
+  </span>
+}
+
+const SessionSettingsContainer = styled.div`
+  display: flex;
+  gap: 14px;
+`
 
 export const NavLinks: React.FC = () => {
+  const { t } = useTranslation();
+
+  const user = null;
+
   return (
     <>
-      {headerLinks.map((link) => (
-        <P.NavLink key={link.text} href={link.href}>{link.text}</P.NavLink>
+      {headerLinks.map((header) => (
+        <P.NavLink key={header} href={header}>{t(`headerLinks.${header}`).toLocaleUpperCase()}</P.NavLink>
       ))}
-      <LanguageMenu />
+
+      {user && <P.NavLink href={'/users'}>{t('headerLinks.userManagement').toLocaleUpperCase()}</P.NavLink>}
+
+      <SessionSettingsContainer>
+        <SessionButtons />
+        <LanguageMenu />
+      </SessionSettingsContainer>
     </>
   );
 };
--- src/constants/HeaderLinks.ts
+++ src/constants/HeaderLinks.ts
@@ -1,32 +1,3 @@
-const headerLinks = [
-  {
-    href: 'https://www.smart-it.com/pl/industries/',
-    text: 'Usługi',
-  },
-  {
-    href: 'https://www.smart-it.com/pl/industries/',
-    text: 'Produkty',
-  },
-  {
-    href: 'https://www.smart-it.com/pl/industries/',
-    text: 'Klienci',
-  },
-  {
-    href: 'https://www.smart-it.com/pl/industries/',
-    text: 'Firma',
-  },
-  {
-    href: 'https://www.smart-it.com/pl/industries/',
-    text: 'Skontaktuj się z nami',
-  },
-  {
-    href: 'https://www.smart-it.com/pl/industries/',
-    text: 'Oferty pracy',
-  },
-  {
-    href: 'https://www.smart-it.com/pl/industries/',
-    text: 'Staż',
-  },
-];
+const headerLinks = ['main', 'about'];
 
 export default headerLinks;
