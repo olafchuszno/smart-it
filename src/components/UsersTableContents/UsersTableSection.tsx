ce464c0c9ac836ea324ce811cc3bd5dddbda88c5
Olaf <olaf.chuszno@gmail.com>
2024-12-02 14:35:33 +0100

    Implement user table message translations

--- src/components/UsersTableContents/UsersTableSection.tsx
+++ src/components/UsersTableContents/UsersTableSection.tsx
@@ -5,19 +5,21 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../app/store.ts';
 import { UsersStatus } from '../../features/users.ts';
 import * as P from './UsersTableSection.parts.tsx';
+import { useTranslation } from 'react-i18next';
 
 const UsersTableContents = () => {
+  const {t} = useTranslation();
   const { status } = useSelector((state: RootState) => state.users);
 
   const { sortedUsers: users } = useSelector((state: RootState) => state.users);
 
   const isFetchingUsers = status === UsersStatus.Fetching;
-  const didUsersFetchingFailed = status === UsersStatus.Error;
+  const hasFetchingError = status === UsersStatus.Error;
   const hasAnyUsers = !!users.length;
 
   return (
     <>
-      {didUsersFetchingFailed && <p>Could not fetch the users</p>}
+      {hasFetchingError && <P.FetchingErrorMessage>{t('usersTable.fetchingErrorMessage')}</P.FetchingErrorMessage>}
 
       {isFetchingUsers && (
         <P.SpinnerContainer>
@@ -25,12 +27,12 @@ const UsersTableContents = () => {
         </P.SpinnerContainer>
       )}
 
-      {!isFetchingUsers &&
+      {!isFetchingUsers && !hasFetchingError &&
         (hasAnyUsers ? (
           <UsersTableUI users={users} />
         ) : (
           <P.NoUsersResultMessage>
-            Nie znaleziono użytkownika.
+            {t('usersTable.notFoundMessage')}
           </P.NoUsersResultMessage>
         ))}
     </>
