af82f1091983b54441d2ecff912eef699f541bd8
Olaf <olaf.chuszno@gmail.com>
2024-12-02 17:04:07 +0100

    Style language menu

--- src/components/LanguageMenu/LanguageMenu.parts.tsx
+++ src/components/LanguageMenu/LanguageMenu.parts.tsx
@@ -2,5 +2,8 @@ import styled from "styled-components";
 
 export const LanguageMenu = styled.select`
   background-color: ${({theme}) => theme.colors.backgrounds.LanguageMenu.one};
-  color: ${({theme}) => theme.colors.text.LanguageMenu.one};
+  color: ${({ theme }) => theme.colors.text.LanguageMenu.one};
+  max-height: 40px;
+  display: flex;
+  align-items: center;
 `
