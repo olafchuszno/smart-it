c11ca0a5a16c0b290e51be5477855da07b2b3669
Olaf <olaf.chuszno@gmail.com>
2024-12-03 18:14:25 +0100

    Create About Page

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,18 @@
+import Text from 'components/generics/Text/Text';
+import React from 'react';
+import { Main } from 'styles/components/Main';
+
+const About = () => {
+  return (
+    <Main>
+      <Text>
+        This is a very long history of the company. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Dolores modi itaque, optio ex fugiat maxime
+        tempora commodi animi deleniti provident eligendi fuga, nulla in corrupti
+        ullam maiores magnam, consequuntur quae!
+      </Text>
+    </Main>
+  );
+};
+
+export default About;
