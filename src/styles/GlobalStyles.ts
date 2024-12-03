c8b9519a185cd0a607aad0f85cedac927e577eaf
Olaf <olaf.chuszno@gmail.com>
2024-12-03 15:18:10 +0100

    Update Global Styles autofill styles

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -6,8 +6,23 @@ export const GlobalStyles = createGlobalStyle`
     background-color: ${({ theme }) => theme.colors.backgrounds.App.one};
   }
 
+  input:-internal-autofill-selected {
+    background-color: unset !important;
+  }
+
+  input:-webkit-autofill {
+    -webkit-box-shadow:0 0 0 50px ${({theme}) => theme.colors.backgrounds.App.one} inset; /* Change the color to your own background color */
+    -webkit-text-fill-color: ${({theme}) => theme.colors.text.GlobalStyles.one};
+}
+
+input:-webkit-autofill:focus {
+    -webkit-box-shadow: 0 0 0 50px ${({theme}) => theme.colors.backgrounds.App.one} inset;/*your box-shadow*/
+    -webkit-text-fill-color: ${({theme}) => theme.colors.text.GlobalStyles.one};
+} 
+
   button {
     background-color: transparent;
     border: none;
+    cursor: pointer;
   }
 `;
