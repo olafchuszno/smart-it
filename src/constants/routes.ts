534bd637962951b4511e4901ef67b5b1d892f425
Olaf <olaf.chuszno@gmail.com>
2024-12-03 18:14:33 +0100

    Create routes file

---
+++ src/constants/routes.ts
@@ -0,0 +1,20 @@
+export const routes = {
+  main: {
+    headerTitle: 'Main Page',
+    href: '/',
+  },
+  about: {
+    headerTitle: 'About Page',
+    href: '/about',
+  },
+  'users-management': {
+    headerTitle: 'Users Management Page',
+    href: '/users-management',
+  },
+  login: {
+    headerTitle: 'Login Page',
+    href: '/login',
+  },
+};
+
+
