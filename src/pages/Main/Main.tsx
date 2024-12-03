015450e1e81830aabbdb2e10a793c13aa789c9de
Olaf <olaf.chuszno@gmail.com>
2024-12-03 18:14:56 +0100

    Create Main Page

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,10 @@
+import Text from 'components/generics/Text/Text'
+import React from 'react'
+
+const MainPage = () => {
+  return (
+    <Text>This is the landing page! Cool design and stuff.</Text>
+  )
+}
+
+export default MainPage
