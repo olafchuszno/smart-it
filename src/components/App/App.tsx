35c496ddc2418d96e4d0ae4490534c9614b87c1f
Olaf <olaf.chuszno@gmail.com>
2024-12-04 11:57:57 +0100

    Use MainLayout component in App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,46 +1,24 @@
 // core
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Navigate,
-  Outlet,
   Route,
   RouterProvider,
 } from 'react-router';
 // components
-import { Header } from '../Header';
 import Login from 'pages/Login/Login.tsx';
 import Logout from 'pages/Logout/Logout.tsx';
 import ErrorPage from 'pages/ErrorPage/ErrorPage.tsx';
 import UsersManagement from 'pages/UsersManagement/UsersManagement.tsx';
-// state
-import { useThemeContext } from '../../contexts/ThemeContext.tsx';
 // styles
-import { GlobalStyles } from 'styles/GlobalStyles.ts';
-import * as P from './App.parts.tsx';
 import About from 'pages/About/About.tsx';
 import MainPage from 'pages/Main/Main.tsx';
 import { routes } from 'constants/routes.ts';
 import PrivateRoute from 'components/PrivateRoute/PrivateRoute.tsx';
+import MainLayout from 'pages/MainLayout/MainLayout.tsx';
 import { useAuthContext } from 'contexts/AuthContext.tsx';
-
-const MainLayout = () => {
-  const { theme } = useThemeContext();
-
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-
-      <P.App>
-        <Header />
-
-        <Outlet />
-      </P.App>
-    </ThemeProvider>
-  );
-};
 
 export const App: React.FC = () => {
   const { isLoggedIn } = useAuthContext();
